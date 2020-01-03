67c86f6405edaebf372166c40a383009b02ba7a3
saif.siddiqui <saif.siddiqui@iproperty.com>
2020-01-04 01:06:41 +0800

    Fetching API with Python

---
+++ src/getPhotos.py
@@ -0,0 +1,7 @@
+import requests
+url = "https://jsonplaceholder.typicode.com/photos"
+
+response = requests.get(url)
+print(response.json())
+#response = requests.request("GET", url)
+#print(response)
