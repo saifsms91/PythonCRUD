1ac61d09bc3f43f7f355ab1ca3426864bee10632
saifsms91 <saifsms91@rediffmail.com>
2020-01-05 19:24:23 +0800

    postApi

--- src/postPhoto.py
+++ src/postPhoto.py
@@ -1,8 +1,6 @@
 
 import requests
 url = 'https://jsonplaceholder.typicode.com/photos'
-
-
 jsonPayLoad = {'albumId':1,'title':'testabc', 'url': 'abc.com','thumbnailUrl':'nothing.com'}
 response = requests.post(url,json=jsonPayLoad)
 print(response.json())
