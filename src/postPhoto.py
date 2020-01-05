9d75807a3c1110a036cfce6cb7a38d028e4af15d
saifsms91 <saif.siddiqui@iproperty.com>
2020-01-05 19:12:59 +0800

    postApi

---
+++ src/postPhoto.py
@@ -0,0 +1,8 @@
+
+import requests
+url = 'https://jsonplaceholder.typicode.com/photos'
+
+
+jsonPayLoad = {'albumId':1,'title':'testabc', 'url': 'abc.com','thumbnailUrl':'nothing.com'}
+response = requests.post(url,json=jsonPayLoad)
+print(response.json())
